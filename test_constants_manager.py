d1def5f4eb7850cd4655d8d6a4edb50a412f6136
studio3104 <studio3104.com@gmail.com>
2016-01-02 19:41:39 +0900

    apply pep8 (avoid `blank line contains whitespace [W293]`)

--- test_constants_manager.py
+++ test_constants_manager.py
@@ -4,38 +4,38 @@ import os
 class TestConstantsManager():
     def test_get_default_value(self):
         __expected = 'val_only_default'
-    
+
         consts = ConstantsManager()
         assert __expected == consts.get('val_only_default')
-    
+
     def test_get_section_value(self):
         __expected = 'val_only_dev'
-    
+
         os.environ["ENV"] = 'dev'
         consts = ConstantsManager()
         assert __expected == consts.get('val_only_dev')
-    
+
     def test_get_default_value_witout_section_value(self):
         __expected = 'val_only_default'
-    
+
         os.environ["ENV"] = 'dev'
         consts = ConstantsManager()
         assert __expected == consts.get('val_only_default')
-    
+
     def test_change_os_environment(self):
         __expected = 'val_only_default'
-    
+
         os.environ["ENV_DUMMY"] = 'dev'
         consts = ConstantsManager(constants_name='ENV_DUMMY')
         assert __expected == consts.get('val_only_default')
-    
+
     def test_using_not_exists_os_environment(self):
         __expected = 'val_only_default'
-    
+
         os.environ["ENV_DUMMY"] = 'dev'
         consts = ConstantsManager()
         assert __expected == consts.get('val_only_default')
-    
+
     def test_using_not_exists_os_environment_and_not_find_key(self):
         os.environ["ENV_DUMMY"] = 'dev'
         consts = ConstantsManager(constants_name='DUMMY')
